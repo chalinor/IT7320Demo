97c5faeef8551143a448851258f8cf9ad5e554d1
chalinorbaliuag <chalinorbaliuag@156.59.108.126>
2015-07-30 15:38:23 +1200

    Second commit for Sample.java

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -15,7 +15,8 @@ public class Sample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Sample output");
+       System.out.println("This will be the second commit");
     }
     
 }
