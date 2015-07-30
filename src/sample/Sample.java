d2e5fad2dd4d2ce9c0cf5e3368198ea04b3b16fb
chalinor <chalinor.baliuag@weltec.ac.nz>
2015-07-30 15:46:43 +1200

    Update Sample.java
    commit within github

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -17,6 +17,7 @@ public class Sample {
     public static void main(String[] args) {
        System.out.println("Sample output");
        System.out.println("This will be the second commit");
+       System.out.println("Commit done inside github");
     }
     
 }
